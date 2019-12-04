aa56c382acbc92fcff8def41a869d0ee4f053734
katsura <huifengbei@gmail.com>
2019-12-04 23:24:07 +0800

    Search and insert operation of BPlus tree.

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,15 @@
+
+import BTree.{BPlusTree, Node}
+
+object Main {
+	def main(args: Array[String]): Unit = {
+		/*
+		val n: BTree.Node = new BTree.Node(3)
+		for(i <- n.point) {
+			println(i.point)
+		}
+		*/
+
+
+	}
+}
